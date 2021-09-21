02fe9ff2f92e47e09385d8373bbb70680f23e95e
Paweł <rosiu978@gmail.com>
2021-09-22 01:52:40 +0200

    Adding Modal functionality

--- script.js
+++ script.js
@@ -3,6 +3,40 @@ let map;
 const menu = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".header__nav__links");
 const menuActive = document.querySelector(".menu-btn active");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+const modalContent = [
+  {
+    image: "./images/box3.png",
+    title: "Kategoria B",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do tempor incididunt ut labore et dolore magna aliqua.",
+    list: [
+      "Ukończone 18 lat",
+      "Badania lekarskie",
+      "Profil Kandydata na Kierowce(PKK)",
+    ],
+  },
+
+  {
+    image: "./images/box2.png",
+    title: "Kategoria D",
+    text: "Lorem ipsum consectetur adipisicing elit, sed do tempor incididunt ut labore et dolore.",
+    list: [
+      "Ukończone 18 lat",
+      "Badania lekarskie",
+      "Profil Kandydata na Kierowce(PKK)",
+    ],
+  },
+
+  {
+    image: "./images/box1.png",
+    title: "Kategoria C + E",
+    text: "Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore et dolore magna aliqua.",
+    list: ["Ukończone 18 lat", "Badania lekarskie"],
+  },
+];
 
 const openMenu = function () {
   navigation.classList.toggle("active");
@@ -10,6 +44,72 @@ const openMenu = function () {
 };
 
 menu.addEventListener("click", openMenu);
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", function (e) {
+    //openModal
+    e.preventDefault();
+    modal.innerHTML = `
+    <button class="close-modal">&times;</button>
+    <div class="modal__main">
+      <div class="modal__main__image">
+        <img src="${modalContent[i].image}" alt="kategoria-b">
+      </div>
+      <div class="modal__main__title">
+        <h1 class="h1-primary">${modalContent[i].title}</h1>
+      </div>
+      
+      <div class="modal__main__text">
+        <p>
+        ${modalContent[i].text}
+        </p>
+      </div>
+      <div class="modal__main__list">
+        <ul>
+        ${modalContent[i].list
+          .map(
+            (item) =>
+              `<li><span>✅</span>
+            ${item}
+          </li>`
+          )
+          .join(" ")}
+        </ul>
+      </div>
+
+
+    </div>
+`;
+    document
+      .querySelector(".close-modal")
+      .addEventListener("click", closeModal);
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+
+    // add text
+  });
+}
+
+// btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  // console.log(event.key) Pokazuje ,ktory klawisz naciskamy
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
